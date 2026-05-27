dd7dccc956414a4779e3cb330fe6886e05b72b13
notesbyadi@gmail.com <notesbyadi@gmail.com>
2026-05-27 14:28:04 +0000

    Created Form1.RowTemplate1 and NOSEn-logo-logo.png; Edited settings and Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -8,3 +8,8 @@ class Form1(Form1Template):
 
     # Any code you write here will run before the form opens.
 
+  @handle("outlined_button_1", "click")
+  def outlined_button_1_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    self.data_grid_1.visible = True
+
