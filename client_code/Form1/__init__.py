a3475f19bc955eecc7346f7eeffd9f93ab09154a
notesbyadi@gmail.com <notesbyadi@gmail.com>
2026-05-28 23:13:24 +0000

    Edited Form1.RowTemplate1 and Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -11,10 +11,6 @@ class Form1(Form1Template):
     super().__init__(**properties)
 
     # Any code you write here will run before the form opens.
-    rows = app_tables.perfumes.search()
-
-    self.repeating_panel_1.items = rows
-
   @handle("outlined_button_1", "click")
   def outlined_button_1_click(self, **event_args):
     """This method is called when the button is clicked"""
