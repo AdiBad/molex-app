37701ef3b3480e5fccf58501b8ca39b1f036aab7
notesbyadi@gmail.com <notesbyadi@gmail.com>
2026-05-28 23:06:55 +0000

    Created Form1.table_rows and Form1.ItemTemplate1; Edited settings, Form1.RowTemplate1 and Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -4,12 +4,16 @@ import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
 
+
 class Form1(Form1Template):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     super().__init__(**properties)
 
     # Any code you write here will run before the form opens.
+    rows = app_tables.perfumes.search()
+
+    self.repeating_panel_1.items = rows
 
   @handle("outlined_button_1", "click")
   def outlined_button_1_click(self, **event_args):
---
+++ client_code/Form1/table_rows/__init__.py
@@ -0,0 +1,12 @@
+from ._anvil_designer import table_rowsTemplate
+from anvil import *
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
+
+class table_rows(table_rowsTemplate):
+  def __init__(self, **properties):
+    # Set Form properties and Data Bindings.
+    super().__init__(**properties)
+
+    # Any code you write here will run before the form opens.
