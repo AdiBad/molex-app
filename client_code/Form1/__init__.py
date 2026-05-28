be3a2cae3206a602d34e4e27d28bb9e7a96e136c
notesbyadi@gmail.com <notesbyadi@gmail.com>
2026-05-28 23:16:19 +0000

    Edited Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -8,6 +8,7 @@ from anvil.tables import app_tables
 class Form1(Form1Template):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
+    print(**properties)
     super().__init__(**properties)
 
     # Any code you write here will run before the form opens.
