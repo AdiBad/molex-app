3e8c0a7cb032e97150af25dbcb2d7bb4366a6da2
notesbyadi@gmail.com <notesbyadi@gmail.com>
2026-05-29 00:49:36 +0000

    Created Form1.ItemTemplate1 and Form1.ItemTemplate2; Edited settings, Form1.RowTemplate1 and Form1

--- client_code/Form1/RowTemplate1/__init__.py
+++ client_code/Form1/RowTemplate1/__init__.py
@@ -1,9 +1,15 @@
 from ._anvil_designer import RowTemplate1Template
 from anvil import *
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
 
 class RowTemplate1(RowTemplate1Template):
   def __init__(self, **properties):
-    # Set Form properties and Data Bindings.
-    super().__init__(**properties)
-
-    # Any code you write here will run before the form opens.
+    self.init_components(**properties)
+    
+    self.label_6.text = self.item['brand']
+    self.label_8.text = self.item['model']
+    self.label_10.text = self.item['similarity']
+    self.text_box_1.text = self.item['chemical_notes']
+    
--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -1,5 +1,6 @@
 from ._anvil_designer import Form1Template
 from anvil import *
+from anvil.tables import app_tables
 
 class Form1(Form1Template):
   def __init__(self, **properties):
@@ -7,9 +8,25 @@ class Form1(Form1Template):
     super().__init__(**properties)
 
     # Any code you write here will run before the form opens.
+    rows = app_tables.perfumes.search()
+
+    # Create dropdown items
+    self.drop_down_1.items = [
+      (row['brand'], row) for row in rows
+    ]
+    self.drop_down_2.items = [
+      (row['model'], row) for row in rows
+    ]
+    self.repeating_panel_1.visible=False
+
+    
 
   @handle("outlined_button_1", "click")
   def outlined_button_1_click(self, **event_args):
     """This method is called when the button is clicked"""
-    self.data_grid_1.visible = True
+    self.rich_text_2.visible=True
+    self.rich_text_3.visible=True
+    self.repeating_panel_1.visible=True
+    self.repeating_panel_1.items = app_tables.chemical_notes.search()
+
 
