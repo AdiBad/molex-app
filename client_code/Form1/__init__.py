4e7c6a7e802272ad3ba6e4f27a80dc00db36af94
notesbyadi@gmail.com <notesbyadi@gmail.com>
2026-06-10 19:36:14 +0000

    Created dot_product; Edited Form1.RowTemplate1, Form1 and Form1.Form2; Deleted Form1.ItemTemplate1 and Form1.ItemTemplate2

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -1,5 +1,6 @@
 from ._anvil_designer import Form1Template
 from anvil import *
+import anvil.server
 from anvil.tables import app_tables
 
 
@@ -26,9 +27,6 @@ class Form1(Form1Template):
 
   @handle("outlined_button_1", "click")
   def outlined_button_1_click(self, **event_args):
-    import pandas as pd
-    a = pd.DataFrame({})
-    print(a)
     perfume_name = self.drop_down_2.selected_value
     perfume_name = self.clean_perfume_name(perfume_name)
 
